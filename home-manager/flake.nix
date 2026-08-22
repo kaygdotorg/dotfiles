18483d055c0d2675f82dd071bdd18085b381f4fb
kayg <mail@kayg.org>
2026-08-22 15:09:40 +0000

    home-manager: pin nixpkgs to mbp registry rev 07e1d92 (aarch64-darwin)

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -2,7 +2,7 @@
   description = "kayg home-manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/07e1d92cdc0ed416cfa11ff3ca40d17e61cfba7a";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
